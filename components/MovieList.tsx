ff7984b87b124e1df09f04a41afa5cf95cc19c38
Mit Donga <meetdonga5344@gmail.com>
2024-05-11 19:27:27 +0530

    Fetching movie program accounts/reviews

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,14 +1,35 @@
 import { Card } from './Card'
 import { FC, useEffect, useState } from 'react'
 import { Movie } from '../models/Movie'
+import { useConnection } from '@solana/wallet-adapter-react'
+import { PublicKey } from '@solana/web3.js'
+interface MovieProgramData {
+	publicKey: string,
+	data: Movie
+}
 
 const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
 
 export const MovieList: FC = () => {
     const [movies, setMovies] = useState<Movie[]>([])
+		const { connection } = useConnection()
+
+		async function getProgramAccounts() {
+			const rawData = await connection.getProgramAccounts(new PublicKey(MOVIE_REVIEW_PROGRAM_ID))
+			
+			const movies: Movie[] = []
+			rawData.forEach((e) => {
+				var decodedData = Movie.deserialize(e.account.data)
+				if (decodedData !== null) {
+					movies.push(decodedData)
+				}
+			})
+			setMovies(movies)
+		}
 
     useEffect(() => {
-        setMovies(Movie.mocks)
+				getProgramAccounts()
+        // setMovies(Movie.mocks)
     }, [])
     
     return (
