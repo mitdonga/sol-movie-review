0848524e8173989179eeb9c632ce4dc7ed1f3156
Mit Donga <meetdonga5344@gmail.com>
2024-05-12 17:32:53 +0530

    Added pagination, sorting, and filtering to Movie list

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,25 +1,52 @@
 import { Card } from './Card'
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { Movie } from '../models/Movie'
 import { useConnection } from '@solana/wallet-adapter-react'
 import { PublicKey } from '@solana/web3.js'
-interface MovieProgramData {
-	publicKey: string,
-	data: Movie
-}
+import { Center, Input } from '@chakra-ui/react'
+import bs58 from 'bs58'
 
 const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
+const MOVIE_REVIEW_ACCOUNT = new PublicKey(MOVIE_REVIEW_PROGRAM_ID)
 
 export const MovieList: FC = () => {
     const [movies, setMovies] = useState<Movie[]>([])
 		const { connection } = useConnection()
+		const [page, setPage] = useState(1)
+		const [query, setQuery] = useState("")
+		const [prgAccountPubKeys, setPrgAccountPubKeys] = useState<PublicKey[]>([])
 
+		async function getPrgAccountPubKeys() {
+			let prgAccounts = await connection.getProgramAccounts(
+				MOVIE_REVIEW_ACCOUNT,
+				{ 
+					dataSlice: {offset: 6, length: 5},
+					filters: [
+						{
+							memcmp: {offset: 6, bytes: bs58.encode(Buffer.from(query))}
+						}
+					]
+			  }
+			)
+			let prgAccountArr = prgAccounts.slice()
+			prgAccountArr.sort((a, b) => {
+				const dataA = a.account.data
+				const dataB = b.account.data
+				return dataA.compare(dataB)
+			})
+			const accountPubKeys = prgAccountArr.map(a => a.pubkey)
+			setPage(1)
+			setPrgAccountPubKeys([...accountPubKeys])
+		}
 		async function getProgramAccounts() {
-			const rawData = await connection.getProgramAccounts(new PublicKey(MOVIE_REVIEW_PROGRAM_ID))
-			
+			const perPage = 10
+			const paginatedAccounts = prgAccountPubKeys.slice((page-1)*perPage, page*perPage)
+			const paginatedAccountData = await connection.getMultipleAccountsInfo(paginatedAccounts)
+
 			const movies: Movie[] = []
-			rawData.forEach((e) => {
-				var decodedData = Movie.deserialize(e.account.data)
+			paginatedAccountData.forEach((e) => {
+				if (e === null) return
+				var decodedData = Movie.deserialize(e.data)
 				if (decodedData !== null) {
 					movies.push(decodedData)
 				}
@@ -28,11 +55,32 @@ export const MovieList: FC = () => {
 		}
 
     useEffect(() => {
-				getProgramAccounts()
-        // setMovies(Movie.mocks)
+			getPrgAccountPubKeys()
     }, [])
+
+		useEffect(() => {
+			getPrgAccountPubKeys()
+		}, [query])
+
+		useEffect(() => {
+			getProgramAccounts()
+		}, [page, prgAccountPubKeys])
     
     return (
+			<>
+				<div>
+					<Center>
+						<Input
+							id='search'
+							color='gray.400'
+							onChange={event => setQuery(event.currentTarget.value)}
+							placeholder='Search'
+							w='97%'
+							mt={2}
+							mb={2}
+						/>
+					</Center>
+				</div>
         <div>
             {
                 movies.map((movie, i) => {
@@ -42,5 +90,10 @@ export const MovieList: FC = () => {
                 })
             }
         </div>
+				<div className='movie-page-btn-container'> 	
+					<button disabled={page === 1} onClick={() => setPage(page - 1)}>Previous</button>
+					<button onClick={() => setPage(page + 1)}>Next</button>
+				</div>
+			</>
     )
 }
